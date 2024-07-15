a5a895d95855aaf88c72a133e6c3f11b2dc800e3
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-07-15 17:34:36 +0700

    complete user

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDb.js";
 import cookieParser from "cookie-parser";
+import router from "./routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -10,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse JSON data in the req.body
-app.use(express.urlencoded({extended: true})) // To parse from data in the req.body
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true })); // To parse from data in the req.body
+app.use(cookieParser());
 
-app.listen(5000, () =>
-  console.log(`Server started at https://localhost:${PORT}`)
+app.use("/api/users", router);
+
+app.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
 );
